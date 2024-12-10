71053eb101ee093ef168316c338637f2ddbc4286
Vijay Kodru <123325232+vijaykodru@users.noreply.github.com>
2024-12-10 10:10:08 +0000

    code1

--- code.js
+++ code.js
@@ -1,9 +1,14 @@
-function factorial(n) {
-    if(n === 0) return 1;
-    else return n * factorial(n - 1);
-}
-
 function e(n) {
-    if(n === 0) return 1;
-    else return 1.0 / factorial(n) + e(n - 1);
+    // Start with the first term, which is 1
+    let result = 1.0;
+    // 0! is 1
+    let factorial = 1;
+    //bottom up approach to find the value incrementing as we go
+    for (let i = 1; i <= n; i++) {
+        // Update the factorial for each i
+        factorial *= i; 
+         // Add the current term to the sum
+        result += 1.0 / factorial;
+    }
+    return result;
 }
